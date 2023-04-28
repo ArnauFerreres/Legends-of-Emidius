3e9ddf21bae9ab6e854ce9407e08d9b68f1a94cf
ArnauFerreres <91682781+ArnauFerreres@users.noreply.github.com>
2023-04-28 21:16:21 +0200

    Movimiento camara a la puerta (palanca)

--- Legend of Emidius/Assets/Scripts/PuzzleController.cs
+++ Legend of Emidius/Assets/Scripts/PuzzleController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using Cinemachine;
 public class PuzzleController : MonoBehaviour
 {
     public enum puzzleType { Generic, Door}
     [SerializeField] private puzzleType currentPuzzleType;
+
+    [Header("Door Puzzle settings")]
+    [SerializeField] private CinemachineFreeLook playerCamera;
+    [SerializeField] private CinemachineVirtualCamera doorPuzzleCamera;
 
     public void finishPuzzle()
     {
@@ -15,10 +19,29 @@ public class PuzzleController : MonoBehaviour
             case puzzleType.Generic:
                 break;
             case puzzleType.Door:
-                gameObject.GetComponent<ItemAnimationController>().enabled = true; ;
+                gameObject.GetComponent<ItemAnimationController>().enabled = true;
+                StartCoroutine(ResetCameraPriority(playerCamera.m_XAxis.m_MaxSpeed, playerCamera.m_YAxis.m_MaxSpeed));
+                playerCamera.Priority = 0;
+                doorPuzzleCamera.Priority = 1;
+                playerCamera.m_XAxis.m_MaxSpeed = 0;
+                playerCamera.m_YAxis.m_MaxSpeed = 0;
+                Time.timeScale = 0;
                 break;
         }
     }
+
+    IEnumerator ResetCameraPriority(float xAxis, float yAxis)
+    {
+        yield return new WaitForSecondsRealtime(3);
+        playerCamera.Priority = 1;
+        doorPuzzleCamera.Priority = 0;
+
+        yield return new WaitForSecondsRealtime(1);
+        playerCamera.m_XAxis.m_MaxSpeed = xAxis;
+        playerCamera.m_YAxis.m_MaxSpeed = yAxis;
+        Time.timeScale = 1;
+    }
+
     private void OnEnable()
     {
         UIController.onUpdateCoins += finishPuzzle;
