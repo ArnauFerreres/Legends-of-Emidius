448c6de190290896cc32b6e6836c8551c98f5cc8
ArnauFerreres <91682781+ArnauFerreres@users.noreply.github.com>
2023-05-03 17:37:20 +0200

    First level pass level

--- Legend of Emidius/Assets/Scripts/PassLevel.cs
+++ Legend of Emidius/Assets/Scripts/PassLevel.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class PassLevel : MonoBehaviour
 {
+    public string passLevel;
     // Start is called before the first frame update
     void Start()
     {
