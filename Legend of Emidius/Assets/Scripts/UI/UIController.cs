f322af5c490bde16c1966572e378598ee6b87a8f
CryBabyGetCake <talismant03@gmail.com>
2023-05-04 20:57:03 +0200

    Pause panel

--- Legend of Emidius/Assets/Scripts/UI/UIController.cs
+++ Legend of Emidius/Assets/Scripts/UI/UIController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
@@ -13,12 +14,60 @@ public class UIController : MonoBehaviour
 
     public static Action onUpdateCoins;
 
+    [SerializeField] private GameObject settingsPanel;
+
+    private bool isPaused;
     public void UpdateTotalCoins()
     {
         totalCoins++;
         coinsTextCounter.text = totalCoins.ToString();
 
-        if(totalCoins == 1)
+        if (totalCoins == 1)
             onUpdateCoins?.Invoke();
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+        settingsPanel.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+            OpenPanel(settingsPanel);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+            ClosePanel(settingsPanel);
+        }
+    }
+
+    public void OpenPanel(GameObject panel)
+    {
+        panel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+        Time.timeScale = 1.0f;
+        isPaused = false;
+        settingsPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
